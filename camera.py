fb72e54e208e16e46e162cfca00dd802b560af02
Giovanni Lenoci <giovanni.lenoci@facile.it>
2018-11-27 00:08:34 +0100

    Adds camera module with configurable interval for timelapse and upload last photo on server

--- camera.py
+++ camera.py
@@ -2,6 +2,7 @@ from picamera import PiCamera
 import time
 from datetime import datetime
 import os.path
+import requests
 
 camera = PiCamera()
 camera.rotation = 180
@@ -10,20 +11,37 @@ class Camera:
     def __init__(self, bounceTime):
         self.bounceTimeSeconds = bounceTime
         self.lastAction = time.time()
+        self.moduleName = '[CAMERA] '
 
     def setBasePath(self, basePath):
         self.basePath = basePath
 
-    def updateStatus(self):
-        if(self.lastAction + self.bounceTimeSeconds < time.time()):
+    def setUrl(self, url):
+        self.url = url
+
+    def updateStatus(self, is_day):
+        if(is_day and self.lastAction + self.bounceTimeSeconds < time.time()):
             dest = self.basePath + str(datetime.now().isocalendar()[1])
             if(not os.path.exists(dest)):
                 os.mkdir(dest);
-                print "directory "+ dest + "created"
+                print self.moduleName + "Directory "+ dest + "created"
 
             dest += "/"+str(int(time.time()))+".jpg"
-            print dest
             camera.start_preview()
             time.sleep(2)
             camera.capture(dest)
             camera.stop_preview()
+
+            time.sleep(2)
+            print "[CAMERA] Sending photo to "+self.url
+
+            with open(dest, 'rb') as f:
+                try:
+                    response = requests.post(self.url, files={'last': f})
+                    print self.moduleName + " Response: "+response.text
+                except requests.exceptions.Timeout:
+                    print self.moduleName + "Timeout"
+                except requests.exceptions.TooManyRedirects:
+                    print self.moduleName + "Too many Redirects"
+                except requests.exceptions.RequestException as e:
+                    print self.moduleName + "Connection failed"
